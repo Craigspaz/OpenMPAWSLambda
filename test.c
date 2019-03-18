f727249b0bf996c6e1b7c622fdb26e6fdacc44ca
Craig Ferris <4960869+Craigspaz@users.noreply.github.com>
2019-03-17 21:56:05 -0400

    added initial files

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+	int threadCounter = 0;
+	
+	#pragma omp parallel num_threads(4)
+	{
+		int currentThread = omp_get_thread_num();
+		if(currentThread == 0)
+		{
+			printf("Master!\n");
+		}
+		else
+		{
+			printf("Slave!\n");
+		}
+	}
+	return 0;
+}
